9ff91228dc6677e777d62fe6d3b732131b7f3fb3
JsBatista <joaobatistafreitasfilho@gmail.com>
2020-02-28 14:20:40 -0300

    Initial Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>SO RAKE</h1>
+      <h3>mR RoBotNicO</h3>
+    </div>
+  );
+}
+
+export default App;
